a42255a5b494b061f6dc88d048814b13cf597053
SamOdle <sro27@cornell.edu>
2020-04-08 18:27:48 -0400

    search for multiple terms, locales, and pages in one execution

--- MainWebScape.py
+++ MainWebScape.py
@@ -1,13 +1,20 @@
 import ScrapeHelper
 from JobPostModule import JobPost
 
-search_term = 'data science'
+# USER INPUT GOES HERE!
+search_terms = ('data science', 'data center', 'IT')
+search_locales = (('Washington', 'DC'), ('New York', 'NY'), ('Chicago', 'IL'))
+pages = 3
+
+# variables
 master_job_list = []
 
 # specify the url
-target_url = ScrapeHelper.get_indeed_url(search_term, 1, 'Washington', 'DC')
-
-master_job_list.append(ScrapeHelper.get_indeed_urls(target_url))
+for term in search_terms:
+    for locale in search_locales:
+        for page in range(0, pages):
+            target_url = ScrapeHelper.get_indeed_url(term, page, locale[0], locale[1])
+            master_job_list.append(ScrapeHelper.get_indeed_urls(target_url))
 
 for x in master_job_list:
     for y in x:
