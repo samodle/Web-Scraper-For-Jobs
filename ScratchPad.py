8286ff193243b6d6de4d7affb0e45804c34d8504
SamOdle <sro27@cornell.edu>
2020-05-03 19:26:36 -0400

    writing to local MongoDB w/ authentication

--- ScratchPad.py
+++ ScratchPad.py
@@ -1,16 +1,8 @@
 import pandas as pd
 import pymongo
 
-client = pymongo.MongoClient('localhost', 27017)
+client = pymongo.MongoClient('localhost', 27017, username='forkMVP7', password='future0fWork')
 db = client["testdb"]  # makes a test database called "test"
 collection = db["testcoll"]  # makes a collection called "test" in the "test" db
 collection.insert_one({"foo": "bar"})  # add a document
 
-"""
-███████╗ ██████╗ ██████╗ ██╗  ██╗
-██╔════╝██╔═══██╗██╔══██╗██║ ██╔╝
-█████╗  ██║   ██║██████╔╝█████╔╝
-██╔══╝  ██║   ██║██╔══██╗██╔═██╗
-██║     ╚██████╔╝██║  ██║██║  ██╗
-╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝                 
-"""
