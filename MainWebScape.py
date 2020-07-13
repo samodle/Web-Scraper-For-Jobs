878d64d6bf397374710f6970d6dcbcdd682d4ea8
SamOdle <sro27@cornell.edu>
2020-07-13 08:49:26 -0400

    fixed pct complete bug. added readme and config file

--- MainWebScape.py
+++ MainWebScape.py
@@ -7,99 +7,110 @@ from CensusData import CityData as city
 from pymongo import MongoClient
 import logging  # Setting up the loggings to monitor execution time
 from time import time  # To time our operations
+import ForkConfig as Fork
 
 # set up mechanism for timing how long the program takes to execute
 logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt='%H:%M:%S', level=logging.INFO)
 t = time()
 
-# CUSTOMIZE YOUR SCRAPING BY EDITING THESE VARIABLES
-master_list_of_search_terms2 = ['unilever']
-master_list_of_search_terms3 = ['customer support', 'customer experience', 'data center', 'help desk', 'technician',
-                                'retail', 'sales']
-search_terms = [
-    'supervisor', 'manager', 'project', 'developer', 'customer support', 'customer experience', 'data center', 'help desk', 'technician',
-    'slack', 'server', 'recruiter', 'computer', 'product', 'assistant', 'genius', 'retail', 'sales']
-
-custom_search_locations = (('Washington', 'DC'), ('New York', 'NY'), ('Chicago', 'IL'), ('San Jose', 'CA'))
-pages = 5
-num_cities = 30
-jobs_per_csv = 10000
-next_file_save_count = jobs_per_csv
-
+# initialize in process variables
+next_file_save_count = Fork.jobs_per_csv
 search_locations_usa = city.import_city_data()
-# These parameters affect how the scrape will run
-only_do_one = True  # if only_do_one is true, the scrape will query 1 search page
-
-# Select websites to scrape posts from
-scrape_indeed = True
-scrape_monster = True
-scrape_career_builder = False
-
-# only does one or the other (ie csv || db export)
-export_to_csv = False  # as opposed to printing data in the console
-export_to_mongo = True
-
-use_custom_locations = False  # option to enter your own search locations instead of picking n top locations
-
-monster_pages = 10  # monster can't handle more than 10 'pages'
-
-# variables
 master_job_list = []
 jobs_found = 0
+monster_pages = 10  # monster can't handle more than 10 'pages'
 
 # STEP 1: IDENTIFY AND SCRAPE THE URLS TO PREPARE FOR STEP 2
-for term in search_terms:  # for each term specific above, go through the following steps
-    # location agnostic
-    if scrape_indeed:
-        for page in range(0, pages):
+for term in Fork.search_terms:  # for each term specific above, go through the following steps
+
+    # always start with a generic, location agnostic search
+    if Fork.scrape_indeed:
+        for page in range(0, Fork.pages):  # iterate through each page
+            # determine the URL for the desired query
             target_url = Indeed.get_url(term, page)
+
+            # scrape the JDs from that query
             temp_list = Indeed.get_job_posts(target_url, term)
             jobs_found = jobs_found + len(temp_list)
+
+            # add the JDs to our list
             master_job_list.append(temp_list)
             print('Indeed No Location pg ' + str(page + 1) + ', Jobs: ' + str(len(temp_list)))
-    if scrape_monster:
+    if Fork.scrape_monster:
+        # determine the URL for the desired query
         target_url = Monster.get_url(term, monster_pages)
+
+        # scrape the JDs from that query
         temp_list = Monster.get_job_posts(target_url, term)
         jobs_found = jobs_found + len(temp_list)
+
+        # add the JDs to our list of lists
         master_job_list.append(temp_list)
         print('Monster No Location Page' + ', Jobs: ' + str(len(temp_list)))
     # specific locations
     print('Scraping...' + term)
-    if use_custom_locations:
-        for locale in custom_search_locations:
+    if Fork.use_custom_locations:
+        for locale in Fork.custom_search_locations:
             print('        ...' + locale[0] + ' ' + locale[1])
-            if scrape_indeed:
-                for page in range(0, pages):
+
+            if Fork.scrape_indeed:
+                for page in range(0, Fork.pages):  # iterate through each page
+                    # determine the URL for the desired query
                     target_url = Indeed.get_url(term, page, locale[0], locale[1])
+
+                    # scrape the JDs from that query
+                    temp_list = Indeed.get_job_posts(target_url, term)
+                    jobs_found = jobs_found + len(temp_list)
+
+                    # add the JDs to our list of lists
+                    master_job_list.append(temp_list)
+                    print('Indeed             pg ' + str(page + 1) + ', Jobs: ' + str(len(temp_list)))
+
+            if Fork.scrape_monster:
+                # determine the URL for the desired query
+                target_url = Monster.get_url(term, monster_pages, locale[0], locale[1])
+
+                # scrape the JDs from that query
+                temp_list = Monster.get_job_posts(target_url, term)
+                jobs_found = jobs_found + len(temp_list)
+
+                # add the JDs to our list of lists
+                master_job_list.append(temp_list)
+                print('Monster' + ', Jobs: ' + str(len(temp_list)))
+
+    else:  # using Census data for cities, sorted by population
+
+        for city in range(0, Fork.num_cities):
+            print('        ...' + search_locations_usa.loc[city, 'city'] + ' ' + search_locations_usa.loc[
+                city, 'state_id'])
+
+            if Fork.scrape_indeed:
+                for page in range(0, Fork.pages):
+                    # determine the URL for the desired query
+                    target_url = Indeed.get_url(term, page, search_locations_usa.loc[city, 'city'],
+                                                search_locations_usa.loc[city, 'state_id'])
+
+                    # scrape the JDs from that query
                     temp_list = Indeed.get_job_posts(target_url, term)
                     jobs_found = jobs_found + len(temp_list)
                     master_job_list.append(temp_list)
-                    print('Indeed             pg ' + str(page + 1) + ', Jobs: ' + str(len(temp_list)))
-            if scrape_monster:
-                target_url = Monster.get_url(term, monster_pages, locale[0], locale[1])
-                temp_list = Monster.get_job_posts(target_url, term)
-                jobs_found = jobs_found + len(temp_list)
-                master_job_list.append(temp_list)
-                print('Monster' + ', Jobs: ' + str(len(temp_list)))
-    else:  # using Census data for cities, sorted by population
-        for city in range(0, num_cities):
-            print('        ...' + search_locations_usa.loc[city, 'city'] + ' ' + search_locations_usa.loc[
-                city, 'state_id'])
-            if scrape_indeed:
-                for page in range(0, pages):
-                    target_url = Indeed.get_url(term, page, search_locations_usa.loc[city, 'city'],
-                                                search_locations_usa.loc[city, 'state_id'])
-                    temp_list = Indeed.get_job_posts(target_url, term)
-                    master_job_list.append(temp_list)
                     print('Indeed            pg ' + str(page + 1) + ', Jobs: ' + str(len(temp_list)))
-            if scrape_monster:
+
+            if Fork.scrape_monster:
+                # determine the URL for the desired query
                 target_url = Monster.get_url(term, monster_pages, search_locations_usa.loc[city, 'city'],
                                              search_locations_usa.loc[city, 'state_id'])
+
+                # scrape the JDs from that query
                 temp_list = Monster.get_job_posts(target_url, term)
+                jobs_found = jobs_found + len(temp_list)
+
+                # add the JDs to our list of lists
                 master_job_list.append(temp_list)
                 print('Monster Page' + ', Jobs: ' + str(len(temp_list)))
 
-if export_to_csv:
+# if saving the data as a .csv, initialize the .csv writing
+if Fork.export_to_csv:
     now = datetime.datetime.now()
     new_csv_file = open(
         "ForkJobExport-" + str(now.day) + str(now.hour) + str(now.minute) + str(now.second) + ".csv", "w",
@@ -109,11 +120,13 @@ if export_to_csv:
               'Search Term', 'Date Scraped', 'Source']
     writer.writerow(fields)
 
-if export_to_mongo:
-    client = MongoClient('localhost', 27017, username='forkAdmin', password='ForkAdmin123')
+# if saving the database, open db connection
+if Fork.export_to_mongo:
+    client = MongoClient(Fork.MONGO_HOST, Fork.MONGO_PORT, Fork.MONGO_USERNAME, Fork.MONGO_PASSWORD)
     db = client.jobs
     collection = db.job_descriptions
 
+# status update
 print('Scrape complete. Jobs Found: ' + str(jobs_found))
 print('Time to scrape: {} min'.format(round((time() - t) / 60, 2)))
 
@@ -123,16 +136,22 @@ csv_file_count: int = 0
 # STEP 2: SCRAPE COMPLETE JOB DESCRIPTIONS, EXPORT THE SCRAPED JOB DESCRIPTIONS
 for x in master_job_list:
     try:
+        # status update
         job_found_count = job_found_count + len(x)
         pct_complete = round(job_found_count / jobs_found, 3)*100
         print(str(pct_complete) + '% Complete. ' + ' Jobs Saved: ' + str(len(x)) + ', Net: ' + str(job_found_count) + ' / ' + str(jobs_found))
         print('Execution Time: {} min'.format(round((time() - t) / 60, 2)))
-        if export_to_csv:
-            print('csv files generated: ' + str(csv_file_count))
+
+        if Fork.export_to_csv:
+            print('csv files generated: ' + str(csv_file_count))  # status update
+
+            # check if the current csv file has exceeded it's capacity, if so create a new one
             if job_found_count > next_file_save_count:
-                new_csv_file.close()
+                new_csv_file.close()  # this csv file is complete
                 csv_file_count += 1
-                next_file_save_count += jobs_per_csv
+                next_file_save_count += Fork.jobs_per_csv
+
+                # start a new csv file
                 now = datetime.datetime.now()
                 new_csv_file = open(
                     "ForkJobExport-" + str(now.day) + str(now.hour) + str(now.minute) + str(now.second) + "-" + str(
@@ -141,9 +160,12 @@ for x in master_job_list:
                 writer = csv.writer(new_csv_file)
                 writer.writerow(fields)
     except Exception as e:
-        print(str(e))
+        ScrapeHelper.print_error_string(str(e))  # print the error, try the next one
+
+    # iterate through each list of JDs
     for y in x:
         try:
+            # scrape the JD based on the URL from the list
             if y.source is ScrapeHelper.INDEED:
                 Indeed.complete_job_profile(y)
             elif y.source is ScrapeHelper.MONSTER:
@@ -152,7 +174,8 @@ for x in master_job_list:
             print(ScrapeHelper.print_error_string(
                 'Error completing Job Profile. URL: ' + y.url + ' // ' + str(e)))
         else:
-            if export_to_csv:
+            # save to csv
+            if Fork.export_to_csv:
                 try:
                     new_row = [y.job_title, y.company, y.location, y.company_rating, y.salary, y.commitment_level,
                                y.url,
@@ -164,7 +187,9 @@ for x in master_job_list:
                 except Exception as e:
                     print(ScrapeHelper.print_error_string(
                         'csv error: ' + y.job_title + ' ' + y.search_term + ' ' + y.location + str(e)))
-            if export_to_mongo:
+
+            # save to db
+            if Fork.export_to_mongo:
                 new_jd = {
                     "JobTitle": y.job_title,
                     "CompanyID": 0,
@@ -183,7 +208,7 @@ for x in master_job_list:
                 except Exception as e:
                     ScrapeHelper.print_error_string(str(e))
 
-if export_to_csv:
+if Fork.export_to_csv:
     new_csv_file.close()
 
 print('Fork Data Export - 100% Complete.  Execution Time: {} min'.format(round((time() - t) / 60, 2)))
---
+++ README.md
@@ -0,0 +1,41 @@
+# forkwebscraper
+
+Fork Web Scraper requires a ForkConfig.py file with the following parameters:
+
+```python
+  # Application Settings
+    # 'search_terms' will be iterated through to query various job boards
+    search_terms = [
+        'supervisor', 'manager', 'project', 'developer', 'customer support', 'customer experience', 'data center', 'help desk', 'technician',
+        'slack', 'server', 'recruiter', 'computer', 'product', 'assistant', 'genius', 'retail', 'sales']
+    
+    # option to define custom locations not included in the census data
+    custom_search_locations = (('Washington', 'DC'), ('New York', 'NY'), ('Chicago', 'IL'), ('San Jose', 'CA'))
+    
+    # query params
+    pages = 5  # if a job board uses pages, number of pages to pull
+    num_cities = 30  # number of cities to query
+    jobs_per_csv = 10000  # how many jds per csv files (if outputting to csv)
+    
+    # feature toggles
+    only_do_one = True  # if only_do_one is true, the scrape will query 1 search page
+    
+    # Select websites to scrape posts from
+    scrape_indeed = True
+    scrape_monster = True
+    scrape_career_builder = False
+    
+    # choose how to save the data
+    export_to_csv = False
+    export_to_mongo = True
+    
+    use_custom_locations = False  # option to enter your own search locations instead of picking n top locations
+
+  # Database Params
+  MONGO_USERNAME = ''
+  MONGO_PASSWORD = ''
+  MONGO_PORT = ''
+  MONGO_HOST = ''
+  MONGO_JOB_DB = ''
+  MONGO_JOB_COLLECTION = ''
+```
